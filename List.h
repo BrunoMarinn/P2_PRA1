271331ba467e5c88cb3ab0aa828172560f18828c
BrunoMarinn <brumaribai@gmail.com>
2026-01-07 23:39:20 +0000

    P2: ajustado List.h a firmas del PDF

---
+++ List.h
@@ -0,0 +1,18 @@
+#ifndef LIST_H
+#define LIST_H
+
+template <class T>
+class List {
+public:
+    virtual ~List() {}
+
+    virtual T get(int pos) = 0;
+    virtual int size() = 0;
+    virtual int search(T e) = 0;
+    virtual void prepend(T e) = 0;
+    virtual void insert(int pos, T e) = 0;
+    virtual T remove(int pos) = 0;
+};
+
+#endif
+
